84018dd6f83b009dc2d6131583f3efa64e18c56d
jai-2006 <jaicoyh@gmail.com>
2026-01-04 02:40:32 +0000

    Update diary-data.js

--- diary-data.js
+++ diary-data.js
@@ -4,16 +4,16 @@ const diaryData = [
     text: "Day out, bits and pieces filmed.",
     images: [],
     videos: [
-      { embed: "https://archive.org/embed/img-8145_202601?file=IMG_8142.mov" },
-      { embed: "https://archive.org/embed/img-8145_202601?file=IMG_8143.mov" },
-      { embed: "https://archive.org/embed/img-8145_202601?file=IMG_8145.mov" },
-      { embed: "https://archive.org/embed/img-8145_202601?file=IMG_8155.mov" }
+      { src: "https://archive.org/download/img-8145_202601/IMG_8142.mov" },
+      { src: "https://archive.org/download/img-8145_202601/IMG_8143.mov" },
+      { src: "https://archive.org/download/img-8145_202601/IMG_8145.mov" },
+      { src: "https://archive.org/download/img-8145_202601/IMG_8155.mov" }
     ]
   },
   {
     date: "2026-01-02",
     text: "Earlier diary entry content...",
     images: ["images/blog-photo2.jpg"],
-    videos: [],
+    videos: []
   }
 ];
